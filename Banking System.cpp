0441e2654a65a430c76dfa1933d2b2c219986705
Saeed-Soukiah <saeedsoukiah@gmail.com>
2025-03-10 00:19:08 +0300

    Banking System.cpp

--- Banking System.cpp
+++ Banking System.cpp
@@ -1,14 +1,6 @@
 /* 
 Author: Saeed Soukiah 
 Date: December 12, 2024 
-Description: This program simulates a basic banking system. It allows users to perform various banking operations including: 
-- Opening a new account. 
-- Checking the balance of an account. 
-- Making deposits. 
-- Making withdrawals. 
-- Closing an account. 
-- Viewing all accounts in the system. 
-The program uses file I/O to save and retrieve account details, ensuring data persistence between sessions. 
 */
 
 #include <iostream>
